6786cbb1eabea12b126374e49f8c01a5407069e6
Shihlin Lu <shihlinlu@gmail.com>
2017-05-01 13:49:04 -0600

    created templates for conceptual model, persona, and use-case.

---
+++ epic/conceptual-model.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>CONCEPTUAL MODEL</title>
+	</head>
+	<body>
+		<header>CONCEPTUAL MODEL</header>
+
+		<main>
+			<h2>Entities and Attributes</h2>
+			<h3>Entities</h3>
+			<ul>
+				<li>entity 1</li>
+				<li>entity 2</li>
+				<li>entity n</li>
+			</ul>
+			<p>Subject: </p>
+			<p>Verb: </p>
+			<p>Object: </p>
+
+			<ul>Entity 1</ul>
+
+			<h2>Relationships</h2>
+			<ul>
+				<li>relationship 1</li>
+				<li>relationship 2</li>
+			</ul>
+
+
+			// Entity relationship diagram goes here
+		</main>
+	</body>
+</html>
---
+++ epic/use-case.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title> USE-CASE</title>
+	</head>
+	<body>
+		<header>
+			<h1>USE-CASE</h1>
+		</header>
+		<main>
+			<p> Use case</p>
+			<h3>Action path: </h3>
+			<ol>
+				<li>Step 1</li>
+			</ol>
+
+		</main>
+	</body>
+</html>
