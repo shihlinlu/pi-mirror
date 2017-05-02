73907792453df47e96982e34bdb78b715fc59d7f
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-05-01 23:58:49 -0600

    changed a section to use case

--- epic/persona_b.php
+++ epic/persona_b.php
@@ -20,7 +20,7 @@
 		<h2>Attitudes and behaviors:</h2>
 		<p>John is optimistic about his new job, he has wanted to be a professional ethical hacker for Security Inc. for a few years now. He moved to Austin from his small hometown just for this internship despite the fact that his apartment rent is more than his monthly paycheck. Still John feels content in his journey moving forward and hopeful towards the future.</p>
 		<h2>Frustration and Needs:</h2>
-		<p>John showed up for work a little late and noticed all of his office mates were gone. looking around confused he searched for his phone, no messages. Opened his laptop and checked his personal gmail account, no new messages. He panics knowing he doesn't have access to the company's system and schedule yet. Goes to the break room and checks the mirror in the break room. But it's no ordinary mirror... it's a smart mirror, he turns it on with wave of his hand and checks the calender. He see's there is a stand-up meeting scheduled this morning just 2 minutes from now in the upstairs conference room.</p>
+		<p></p>
 	</main>
 	</body>
 </html>
---
+++ epic/use-case_b.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>User Case B</title>
+	</head>
+	<body>
+		<header>
+			<h2>Use Case B: Business/ commercial</h2>
+		</header>
+		<body>
+			<p>John showed up for work a little late and noticed all of his office mates were gone. looking around confused he searched for his phone, no messages. Opened his laptop and checked his personal gmail account, no new messages. He panics knowing he doesn't have access to the company's system and schedule yet. Goes to the break room and checks the mirror in the break room. But it's no ordinary mirror... it's a smart mirror., he turns it on with wave of his hand and checks the calender. He see's there is a stand-up meeting scheduled this morning just 2 minutes from now in the upstairs conference room.</p>
+		</body>
+	</body>
+</html>
