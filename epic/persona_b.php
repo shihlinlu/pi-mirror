a28a2f3770dfe54f8ef16576b39807df3807897b
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-05-01 23:40:27 -0600

    added attitudes, behaviors and a frustrations and needs section

--- epic/persona_b.php
+++ epic/persona_b.php
@@ -9,11 +9,18 @@
 		<h1>Persona B: Business/ Commercial</h1>
 	</header>
 	<main>
-		<h3>Name: John Smith</h3>
+		<h3>Name: John Snow</h3>
 		<h3>Age: 25</h3>
 		<h3>Job: Intern at Security Inc.</h3>
 		<h3>Location: Austin, Texas</h3>
-		<p>Back Story: John recently began work at Security Incorporation as an intern. He recently finished a 10-week Coding Bootcamp and is new to the field. So new in fact he has not yet been given security clearance to have access to the companies files including the meeting schedule. </p>
+		<p>Back Story: John recently began work at Security Incorporation as an intern. He recently finished a 10-week Coding Bootcamp and is new to the field. So new in fact he has not yet been given security clearance to have access to the companies files including the office schedule. </p>
+		<h2>Technology:</h2>
+		<p>John has an iPhone5 and his computer is a Macbook pro both about two years old.</p>
+		<br>
+		<h2>Attitudes and behaviors:</h2>
+		<p>John is optimistic about his new job, he has wanted to be a professional ethical hacker for Security Inc. for a few years now. He moved to Austin from his small hometown just for this internship despite the fact that his apartment rent is more than his monthly paycheck. Still John feels content in his journey moving forward and hopeful towards the future.</p>
+		<h2>Frustration and Needs:</h2>
+		<p>John showed up for work a little late and noticed all of his office mates were gone. looking around confused he searched for his phone, no messages. Opened his laptop and checked his personal gmail account, no new messages. He panics knowing he doesn't have access to the company's system and schedule yet. Goes to the break room and checks the mirror in the break room. But it's no ordinary mirror... it's a smart mirror, he turns it on with wave of his hand and checks the calender. He see's there is a stand-up meeting scheduled this morning just 2 minutes from now in the upstairs conference room.</p>
 	</main>
 	</body>
 </html>
