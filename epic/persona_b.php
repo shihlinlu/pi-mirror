40f48527a4d1038bba540139504997f744ddc8f9
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-05-01 21:27:12 -0600

    Started persona details a little back story

--- epic/persona_b.php
+++ epic/persona_b.php
@@ -1,13 +1,19 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8"
-		<title>
-			Persona B
-	</title>
+		<meta charset="utf-8">
+		<title>Persona B</title>
 	</head>
 	<body>
-		<h1>Persona B: Business/ commercial</h1>
-
+		<header>
+		<h1>Persona B: Business/ Commercial</h1>
+	</header>
+	<main>
+		<h3>Name: John Smith</h3>
+		<h3>Age: 25</h3>
+		<h3>Job: Intern at Security Inc.</h3>
+		<h3>Location: Austin, Texas</h3>
+		<p>Back Story: John recently began work at Security Incorporation as an intern. He recently finished a 10-week Coding Bootcamp and is new to the field. So new in fact he has not yet been given security clearance to have access to the companies files including the meeting schedule. </p>
+	</main>
 	</body>
 </html>
