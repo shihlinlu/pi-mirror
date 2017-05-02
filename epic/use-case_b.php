cb8014b5abf0e9a2d998fb2a417f5a779b801f8f
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-05-02 11:35:20 -0600

    done with use-case-b and interaction flow

--- epic/use-case_b.php
+++ epic/use-case_b.php
@@ -9,11 +9,17 @@
 			<h2>Use Case B: Business/ commercial</h2>
 		</header>
 		<main>
-			<p>John showed up for work a little late and noticed all of his office mates were gone. looking around confused he searched for his phone, no messages. Opened his laptop and checked his personal gmail account, no new messages. He panics knowing he doesn't have access to the company's system and schedule yet. Goes to the break room and checks the mirror in the break room. But it's no ordinary mirror... it's a smart mirror., he turns it on with wave of his hand and checks the calender. He see's there is a stand-up meeting scheduled this morning just 2 minutes from now in the upstairs conference room.</p>
-			<ul>
-				Interaction Flow
-			</ul>
-			<li></li>
+			<p>John showed up for work a little late and noticed all of his office mates were gone. looking around confused he searches for his phone, no messages. Opened his laptop and checked his personal gmail account, no new mail. He panics knowing he doesn't have access to the company's system and schedule yet. Goes to the break room and checks the mirror in the break room. But it's no ordinary mirror... it's a smart mirror., he turns it on with wave of his hand and checks the calender. He see's there is a stand-up meeting scheduled this morning just 2 minutes from now in the upstairs conference room.</p>
+			<h3>Interaction Flow</h3>
+			<ol>
+				<li>John enters the building with his visitor's badge</li>
+				<li>Goes to break room and faces Smart Mirror</li>
+				<li>Waves his right hand in front of the screen from his right to left</li>
+				<li>Smart mirror turns on</li>
+				<li>John opens the calender feature</li>
+				<li>See's that there is a meeting set for today at 8am in the upstairs conference room</li>
+				<li>Checks clock on mirror and see's that the meeting starts in two minutes</li>
+				<li>Mirrors turns off after a few minutes of inactivity</li>
 		</main>
 	</body>
 </html>
