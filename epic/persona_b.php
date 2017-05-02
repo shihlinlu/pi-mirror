bf80dc096a6a2b9e15769f3b7eb1f2f8344d73ae
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-05-02 09:58:42 -0600

    done with first draft of perosna b working on use-case-b now

--- epic/persona_b.php
+++ epic/persona_b.php
@@ -11,16 +11,21 @@
 	<main>
 		<h3>Name: John Snow</h3>
 		<h3>Age: 25</h3>
-		<h3>Job: Intern at Security Inc.</h3>
+		<h3>Job: Intern at Sandia Labs</h3>
 		<h3>Location: Austin, Texas</h3>
-		<p>Back Story: John recently began work at Security Incorporation as an intern. He recently finished a 10-week Coding Bootcamp and is new to the field. So new in fact he has not yet been given security clearance to have access to the companies files including the office schedule. </p>
+		<p>Back Story: John recently began work at Sandia Laboratories as an intern. He recently finished a 10-week Coding Bootcamp and is new to the field. So new in fact he has not yet been given security clearance to have access to the companies files including the office schedule. </p>
 		<h2>Technology:</h2>
 		<p>John has an iPhone5 and his computer is a Macbook pro both about two years old.</p>
 		<br>
 		<h2>Attitudes and behaviors:</h2>
-		<p>John is optimistic about his new job, he has wanted to be a professional ethical hacker for Security Inc. for a few years now. He moved to Austin from his small hometown just for this internship despite the fact that his apartment rent is more than his monthly paycheck. Still John feels content in his journey moving forward and hopeful towards the future.</p>
+		<p>John is optimistic about his new job, he has wanted to be a professional ethical hacker for Sandia LAbs. for a few years now. He moved to Albuquerque from his small hometown just for this internship despite the fact that his apartment rent is more than his monthly paycheck. Still John feels content in his journey moving forward and hopeful towards the future.</p>
 		<h2>Frustration and Needs:</h2>
-		<p></p>
+		<p>John feels a bit frustrated at times because because doing his job well is made difficult by not having a security clearance. Some of the other employee's were saying it takes months to get all the paperwork together before a badge is even printed. He needs an electronic copy of the companies schedule. His boss gave him a copy of last weeks schedule but he quickly learned that day-to-day meetings change times and locations constantly. He feels an very strong sense of impostor syndrome and like he knows nothing.</p>
+		<h3>Goals:</h3>
+		<ol>
+			<li>To see an electronic version of his team's schedule</li>
+			<li>Access to it without special security clearance</li>
+		</ol>
 	</main>
 	</body>
 </html>
