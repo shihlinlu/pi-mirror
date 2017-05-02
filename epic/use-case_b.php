7a0b8d372432c06d15da5ee49877e4a945438ca8
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-05-02 09:24:08 -0600

    added more to persona-b and use-case-b

--- epic/use-case_b.php
+++ epic/use-case_b.php
@@ -10,6 +10,10 @@
 		</header>
 		<main>
 			<p>John showed up for work a little late and noticed all of his office mates were gone. looking around confused he searched for his phone, no messages. Opened his laptop and checked his personal gmail account, no new messages. He panics knowing he doesn't have access to the company's system and schedule yet. Goes to the break room and checks the mirror in the break room. But it's no ordinary mirror... it's a smart mirror., he turns it on with wave of his hand and checks the calender. He see's there is a stand-up meeting scheduled this morning just 2 minutes from now in the upstairs conference room.</p>
+			<ul>
+				Interaction Flow
+			</ul>
+			<li></li>
 		</main>
 	</body>
 </html>
