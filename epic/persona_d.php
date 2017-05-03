00434d7b6ad003b6a650fb1afd041b47b3f2f294
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-05-02 21:59:59 -0600

    Done with back story and tech for persona_d of a Student at the STEMulus center.

--- epic/persona_d.php
+++ epic/persona_d.php
@@ -5,8 +5,25 @@
 		<title>Persona</title>
 	</head>
 	<body>
+		<h1>Persona</h1>
 		<main>
-			<h2>Persona</h2>
+			<h3>Name: Piper Halliwell</h3>
+			<h3>Age: 28</h3>
+			<h3>Current Profession:</h3>
+			<p>Piper is a Deep Dive Coding Bootcamp student Cohort-17. She has been working long hours as the assistant chef in a local restaurant called Quake. Piper wanted to leave the restaurant business and start a career she loves. She has always been very intrigued about the field of computer science and has been working long hours for the past year to save up for tuition and expenses for the 2.5 months of schooling.</p>
+			<h3>Technology:</h3>
+			<ol>
+				<li>Raspberry pi3 Smart Mirror</li>
+				<li>with a 32 gb microSD card</li>
+				<li>NOOBS (new out of the box software) Linux OS</li>
+				<li>Sensly HAT: sensor for indoor pressure, humidity and gas</li>
+				<li>PIR Motion Sensor</li>
+				<li>HC-SR04 Distance Sensor</li>
+			</ol>
+			<h3>Attitudes and Behaviors</h3>
+			<p>Piper is blah</p>
+			<h3>Frustrations and Needs</h3>
+			<p></p>
 		</main>
 	</body>
 </html>
