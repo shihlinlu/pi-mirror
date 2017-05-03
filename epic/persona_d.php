47ad1fd353226314ca3e9e20cd96610b66b6b45b
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-05-02 20:43:29 -0600

    Created Persona and use case d simple html doc

---
+++ epic/persona_d.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Persona</title>
+	</head>
+	<body>
+		<main>
+			<h2>Persona</h2>
+		</main>
+	</body>
+</html>
---
+++ epic/use-case_d.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<meta charset="UTF-8">
+	<title>
+		Use Case
+	</title>
+	<body>
+		<main>
+			<h2>Use Case</h2>
+		</main>
+	</body>
+</html>
