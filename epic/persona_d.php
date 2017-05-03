0d52cbe86ddb6c564cea97956b66fc51fc94ee89
Shihlin Lu <shihlinlu@gmail.com>
2017-05-03 11:59:11 -0600

    Updated persona and use case to make it more: 1) compelling, 2) interesting, and 3) logical. Also fixed grammatical and spelling errors.

--- epic/persona_d.php
+++ epic/persona_d.php
@@ -10,26 +10,28 @@
 			<h3>Name: Piper Halliwell</h3>
 			<h3>Age: 28</h3>
 			<h3>Current Profession:</h3>
-			<p>Piper is a Deep Dive Coding Bootcamp student Cohort-17. </p>
+			<p>Piper is a Deep Dive Coding Bootcamp student Cohort-17.</p>
 			<h3>Technology:</h3>
 			<ol>
-				<li>Raspberry pi3 Smart Mirror</li>
-				<li>with a 32 gb microSD card</li>
+				<li>Smart Mirror powered by Raspeberry Pi 3</li>
+				<li>32 gb microSD card</li>
 				<li>NOOBS (new out of the box software) Linux OS</li>
 				<li>Sensly HAT: sensor for indoor pressure, humidity and gas</li>
 				<li>PIR Motion Sensor</li>
 				<li>HC-SR04 Distance Sensor</li>
+				<li>iPhone 7 with 4G data as a remote control for the smart mirror</li>
 			</ol>
 			<h3>Attitudes and Behaviors</h3>
-			<p>Piper is has been working long hours as the assistant chef in a local restaurant called Quake. She has been dreaming to leave the restaurant business and start a career she can be proud of. She has always been very intrigued about the field of computer science and has been working long hours for the past year to save up for tuition and expenses for the 2.5 months of schooling. She is tired and worn out now she has a large amount of pre-work reading yet to do.</p>
+			<p>For the past 3 years, Piper has been working as the assistant chef in a local restaurant called Quake Cafe. She has been dreaming to leave the restaurant industry and start a career she can be proud of. She has always been very intrigued about the field of computer science, particularly when she attended an invitation-only event at Singularity University (SU). At this event, she met Ray Kurzweil, the founder of SU. At the end of his presentation about reaching Singularity and Moore's Law, she introduced herself to Ray and had a very interesting conversation because she had recently finished watching the first season of Westworld. In addition, she finished reading Kurzweil's "How to Create a Mind", and that ignited her interest in robotics. After her return to Albuquerque, she discovered the Deep Dive Coding Bootcamp and had a serious interest and submitted her application. She has also been working long hours for the past year to save up for tuition and expenses for the 2.5 months, 700+ hours of coding and studying.</p>
 			<h3>Frustrations and Needs</h3>
-			<p>Piper has finished the pre-work and since quitting her job two weeks ago she is stressed, tired and uncertain. Concerned about money, wondering if she can succeed in this field, did she study enough of the pre-work to be a successful student? She also desperately needs to fix her hair if only there was a mirror near by.</p>
+			<p>Piper resigned from her job nearly two months ago. She was accepted to the bootcamp a month prior to that, and made the decision within one day to finalize her registration. Concerned about money and contemplating her success in this field, she promised herself that she will dedicate an extraordinary amount of time (beyond the minimum) on the pre-work to ensure her success in the program. After watching the first two screencasts of the first pre-work assignment, she needs to meet with Dylan and the instructors to set up her MySQL database. Also, it was a super windy day with 50mph wind gusts, therefore she desperately needs to fix her hair.</p>
 			<h3>Goals:</h3>
 			<ol>
-				<li>Got to CNM Stemulus Center and get mysql set up</li>
-				<li>ponder her life decisions</li>
-				<li>Find reassurance that choosing the Deep Dive Bootcamp was worth it</li>
-				<li>Fix hair</li>
+				<li>Arrive to CNM STEMulus Center</li>
+				<li>Fix hair before meeting with instructors</li>
+				<li>Navigate the smart mirror</li>
+				<li>Meet the instructors to discuss her thoughts about the pre-work so far and future career interests</li>
+				<li>Get MySQL set up</li>
 			</ol>
 		</main>
 	</body>
--- epic/use-case_d.php
+++ epic/use-case_d.php
@@ -7,23 +7,23 @@
 	<body>
 		<main>
 			<h2>Use Case</h2>
-			<p>Piper has come in to meet with Dylan to set up mySQL before the course starts. She walks around the CNM Stemulus classroom for the first time. Pondering about spending the next 10 weeks here she wonders if yellow and grey were the best colors. Off to the side she notices a mirror hanging on the wall. Walking towards it to take a moment to fix her hair the mirror suddenly turns on! She is startled mirrors don't turn on!?! She notices the time and outside weather displayed. Piper realized that this is a smart mirror.</p>
+			<p>Piper has come in to meet with Dylan to set up mySQL before the course starts. She walks around the CNM STEMulus Center for the first time. Pondering about spending the next 10 weeks here she wonders if yellow and grey were suitable colors for a productive environment. Off to the side she notices a mirror hanging on the wall. Walking towards it to take a moment to fix her hair, the mirror suddenly turns on! She got startled because since when do mirrors turn on?!?! She then notices the time and outside weather displayed. Dylan tells her that this is a smart mirror that an awesome capstone project team from cohort 16 built. She is then very excited for the program and wants to build her own smart mirror.</p>
 			<h3>Interaction Flow</h3>
 			<ul>
-				<li>Piper walks up to mirror</li>
+				<li>Piper arrives to the STEMulus center and notices a mirror on the cool, laser-cut wall divider in the classroom</li>
 				<li>Motion/Distance sensors detect movement 2-3 feet in front</li>
-				<li>Sensors sends volts to wake up raspberry pi from sleep mode</li>
-				<li>Raspberry pi3 turns on display monitor</li>
-				<li>Raspberry pi displays web page</li>
-				<li>Web page pulls clock information from raspberry pi and displays the time</li>
-				<li>Web page uses weather underground api for Albuquerque, NM</li>
-				<li>Web page displays weather as 82 degrees</li>
-				<li>Piper exclaims "Dam it's hot!"</li>
+				<li>Sensors sends volts to wake up the Raspberry Pi from sleep mode</li>
+				<li>Raspberry Pi turns on the display monitor</li>
+				<li>The web page displays on the mirror</li>
+				<li>Web page pulls clock information and displays the time</li>
+				<li>Web page uses Weather Underground API for Albuquerque, NM</li>
+				<li>Web page displays weather as 82 degrees with 50mph wind gusts</li>
+				<li>Piper exclaims "Wow, it is freaking hot and super windy!"</li>
 				<li>Piper walks away from mirror</li>
-				<li>Motion/ distance sensor does not detect movement or anything in front for over 1 minute signal sent to raspberry pi</li>
+				<li>Motion and distance sensors do not detect movement or object presence for 45 seconds and a signal is sent to the Raspberry Pi</li>
 				<li>Pi turns off display and goes to sleep mode</li>
 			</ul>
-			<p>After Seeing the coolest mirror ever (and fixing her hair) She begins to have a more positive outlook on life and her choice to join the Deep Dive Bootcamp because of the magic mirror and the charming pi-o-mirrors.</p>
+			<p>After Seeing the coolest mirror ever (and fixing her hair) She begins to have a more positive outlook on life and is super happy that she made the choice to enroll in the Deep Dive Bootcamp because of the magic mirror and the charming Pi-O-Mirrors team.</p>
 		</main>
 	</body>
 </html>
