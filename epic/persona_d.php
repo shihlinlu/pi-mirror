b2b0a49883d5543294f0ec30d31fc586d594c5f0
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-05-02 22:23:30 -0600

    Done with first draft of Persona and Use case

--- epic/persona_d.php
+++ epic/persona_d.php
@@ -10,7 +10,7 @@
 			<h3>Name: Piper Halliwell</h3>
 			<h3>Age: 28</h3>
 			<h3>Current Profession:</h3>
-			<p>Piper is a Deep Dive Coding Bootcamp student Cohort-17. She has been working long hours as the assistant chef in a local restaurant called Quake. Piper wanted to leave the restaurant business and start a career she loves. She has always been very intrigued about the field of computer science and has been working long hours for the past year to save up for tuition and expenses for the 2.5 months of schooling.</p>
+			<p>Piper is a Deep Dive Coding Bootcamp student Cohort-17. </p>
 			<h3>Technology:</h3>
 			<ol>
 				<li>Raspberry pi3 Smart Mirror</li>
@@ -21,9 +21,9 @@
 				<li>HC-SR04 Distance Sensor</li>
 			</ol>
 			<h3>Attitudes and Behaviors</h3>
-			<p>Piper is blah</p>
+			<p>Piper is has been working long hours as the assistant chef in a local restaurant called Quake. Piper wanted to leave the restaurant business and start a career she loves. She has always been very intrigued about the field of computer science and has been working long hours for the past year to save up for tuition and expenses for the 2.5 months of schooling. She is tired and worn out now she has a large amount of pre-work to do.</p>
 			<h3>Frustrations and Needs</h3>
-			<p></p>
+			<p>Piper has finished the pre-work and since quitting her job two weeks ago she is stressed. Concerned about money, wondering if she can succeed in this field, did she work enough on the prework to be a successful student?</p>
 		</main>
 	</body>
 </html>
--- epic/use-case_d.php
+++ epic/use-case_d.php
@@ -7,6 +7,20 @@
 	<body>
 		<main>
 			<h2>Use Case</h2>
+			<p>Piper has come in to meet with Dylan to set up mySQL before the course starts. She walks around the CNM Stemulus classroom for the first time. Pondering about spending the next 10 weeks here she wonders if yellow and grey were the best colors. Off to the side she notices a mirror hanging on the wall. Walking towards it to take a moment to fix her hair the mirror suddenly turns on! She is startled mirrors don't turn on!?! She notices the time and outside weather displayed. Piper realized that this is a smart mirror.</p>
+			<h3>Interaction Flow</h3>
+			<ul>
+				<li>Piper walks up to mirror</li>
+				<li>Motion sensors detect movement 2-3 feet in front</li>
+				<li>Sensors sends volts to wake up raspberry pi from sleep mode</li>
+				<li>Raspberry pi3 turns on display monitor</li>
+				<li>Raspberry pi displays web page</li>
+				<li>Web page pulls clock information from raspberry pi and displays the time</li>
+				<li>Web page uses weather underground api for Albuquerque, NM</li>
+				<li>Web page displays weather as 82 degrees</li>
+				<li>Piper exclaims "Dam its hot!"</li>
+			</ul>
+			<p>After Seeing the coolest mirror ever she begins to have a more positive outlook on life and her choice to join the Deep Dive Bootcamp because of the pi-o-mirrors.</p>
 		</main>
 	</body>
 </html>
