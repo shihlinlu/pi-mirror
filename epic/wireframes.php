fdcc3287734377c9c4402ea26f7fe91969b23557
Shihlin Lu <shihlinlu@gmail.com>
2017-05-08 10:00:06 -0600

    updated summary
    https://app.asana.com/0/333821720990064/333821720990065

--- epic/wireframes.php
+++ epic/wireframes.php
@@ -9,13 +9,12 @@
 			<h1>Wireframes 1.0</h1>
 		</header>
 		<main>
-			<h2>Summary</h2>
-			<p>The initial wireframes are based on the standard features that the group collectively decided on, which includes temperature, time, current date, reminders, indoor air quality, and other user-input feeds (i.e., Slack, News). The user can navigate individually to each section; for instance, navigating to the indoor air quality will provide more information such as individual levels of gasses and charts. Further research is needed for user flow and arrangement of modules.</p>
+			<!-- SUMMARY SECTION -->
+			<h2>Initial Sketches</h2>
+			<p>The initial wireframes are based on the standard features that the group collectively decided on, which includes temperature, time, current date, reminders, indoor air quality, and other user-input feeds (i.e., Slack, News). The user can navigate individually to each section through motion gestures; for instance, navigating to the indoor air quality will enable the user to access more information such as individual levels of gasses, detailed notifications, and charts. Next steps include research of the <a href="https://blog.prototypr.io/10-usability-heuristics-with-examples-4a81ada920c">10 Usability Heuristics of User Interface Design</a> to determine the default arrangement of modules.</p>
 			<!-- SECTION 2 -->
 			<section>
 				<img src="wireframe-images/wireframes_v1.png" alt="home screen" />
-
-
 
 			</section>
 
