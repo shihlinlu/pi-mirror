7285af2a83f9b112da1c6f2c47bf86de853f1704
Tucker L <tucker@fellafox.com>
2017-05-10 14:01:56 -0600

    Added composer
    https://app.asana.com/0/332296370771733/337036389641843

---
+++ public_html/templates/home.php
@@ -0,0 +1,12 @@
+
+<main class="bg-info">
+
+	<div class="container-fluid">
+		<div class="row">
+
+			<div class="container center-splash">
+				this is a template
+			</div>
+		</div>
+	</div>
+</main>
---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+	selector: "ng4-example",
+	templateUrl: "./templates/ng4-example-app.php"
+})
+
+export class AppComponent {}
