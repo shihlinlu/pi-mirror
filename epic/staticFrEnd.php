b9315396d2161511a5e5d4a2dbaf6cbd7686259f
S JACKSON <jackson.sabastian@gmail.com>
2017-05-11 16:50:50 -0600

    added img descriptions :-)

--- epic/staticFrEnd.php
+++ epic/staticFrEnd.php
@@ -22,21 +22,21 @@
         <h1>Demo</h1>
     <main>
 
-        <img src="images/clock.jpeg">
-        <img src="images/conditions.jpeg">
+        <img src="images/clock.jpeg" alt="clock">
+        <img src="images/conditions.jpeg" alt="conditions">
 
         <div class="notif">
         <p>(Notification about air)</p>
-        <img src="images/notification.jpeg">
+        <img src="images/notification.jpeg" alt="notifications">
         </div>
 
-        <div class="greeting">
+        <div class="greeting" alt="greeting">
             <em> Good Morning Pi Group</em>
         </div>
 
         <div class="feed">
-        <img src="images/todolist.jpeg">
-        <img src="images/slack.jpeg">
+        <img src="images/todolist.jpeg" alt="todolist">
+        <img src="images/slack.jpeg" alt="slack">
         </div>
 
     </main>
