7de0c160ce94b015f82ee6d4cc062c69c778f060
Shihlin Lu <shihlinlu@gmail.com>
2017-05-11 17:32:47 -0600

    updated SQL
    https://app.asana.com/0/333821720990064/333821720990069

--- sql/pi-mirror.sql
+++ sql/pi-mirror.sql
@@ -4,8 +4,8 @@ DROP TABLE IF EXISTS sensor;
 
 CREATE TABLE sensor (
 	sensorId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	sensorUnit VARCHAR(32) NOT NULL,
-	sensorDescription VARCHAR(200) NOT NULL,
+	sensorUnit VARCHAR(8) NOT NULL,
+	sensorDescription VARCHAR(32) NOT NULL,
 	-- this declares the primary key for the entity
 	PRIMARY KEY (sensorId)
 );
@@ -14,7 +14,7 @@ CREATE TABLE reading (
 	readingId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	readingSensorId INT UNSIGNED NOT NULL,
 	sensorValue DECIMAL(12,6) NOT NULL,
-	sensorDateTime DATETIME(6) NOT NULL,
+	sensorDateTime TIMESTAMP(6) NOT NULL,
 	INDEX (readingSensorId),
 	FOREIGN KEY (readingSensorId) REFERENCES sensor(sensorId),
 	PRIMARY KEY(readingId)
