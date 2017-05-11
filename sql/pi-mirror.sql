0483241accff99c23dccfbd5e0430b0a9a191ce5
Shihlin Lu <shihlinlu@gmail.com>
2017-05-11 16:18:43 -0600

    specified datetime object
    https://app.asana.com/0/333821720990064/334726101376739

--- sql/pi-mirror.sql
+++ sql/pi-mirror.sql
@@ -1,10 +1,10 @@
 --  primary data stored in SQL is indoor air quality from Sensly HAT
-DROP TABLE IF EXISTS environment;
 DROP TABLE IF EXISTS reading;
+DROP TABLE IF EXISTS sensor;
 
 CREATE TABLE sensor (
 	sensorId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	sensorUnit VARCHAR(50) NOT NULL,
+	sensorUnit VARCHAR(32) NOT NULL,
 	sensorDescription VARCHAR(200) NOT NULL,
 	-- this declares the primary key for the entity
 	PRIMARY KEY (sensorId)
@@ -14,7 +14,7 @@ CREATE TABLE reading (
 	readingId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	readingSensorId INT UNSIGNED NOT NULL,
 	sensorValue DECIMAL(12,6) NOT NULL,
-	sensorDateTime DATETIME NOT NULL,
+	sensorDateTime DATETIME(6) NOT NULL,
 	INDEX (readingSensorId),
 	FOREIGN KEY (readingSensorId) REFERENCES sensor(sensorId),
 	PRIMARY KEY(readingId)
