7508744f8dc2dfde886a912da6ad73431f30cfc3
Luc Flynn <lucnmsu@gmail.com>
2017-05-11 15:38:05 -0600

    placed images in first rough draft.
    https://app.asana.com/0/333821720990064/333821720990069

--- epic/staticFrEnd.php
+++ epic/staticFrEnd.php
@@ -1,11 +1,27 @@
 <!DOCTYPE html>
 <html>
+<head>
     <meta charset="UTF-8">
+    <link type="text/css" href="css/style.css" rel="stylesheet" />
+</head>
+
     <title>
         Static Front end
     </title>
 <body>
-    <h1>Demo</h1>
-<main>
-    <p></p>
-</main>
+    <div class="container" >
+        <h1>Demo</h1>
+    <main>
+
+        <img src="images/clock.jpeg">
+        <img src="images/conditions.jpeg">
+        <img src="images/notification.jpeg">
+
+        <div class="greeting">
+            <em> Good Morning Pi Group</em>
+        </div>
+
+    </main>
+    </div>
+</body>
+</html>
