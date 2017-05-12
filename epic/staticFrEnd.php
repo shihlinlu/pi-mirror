5b2a99944b2f4259401287cd41b4dd282b80d536
S JACKSON <jackson.sabastian@gmail.com>
2017-05-12 09:43:14 -0600

    ugly commit - testing rows

--- epic/staticFrEnd.php
+++ epic/staticFrEnd.php
@@ -1,57 +1,64 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <link type="text/css" href="css/style.css" rel="stylesheet" />
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<head>
+		<meta charset="UTF-8">
+		<link type="text/css" href="css/style.css" rel="stylesheet"/>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-	<!-- Optional theme -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
 
-</head>
+	</head>
 
-    <title>
-        Static Front end
-    </title>
-<body>
-	<!-- container for the page -->
-	<div class="container">
-		<div class="row"
-		<div class="col-lg-8 text-center">
+	<title>
+		Static Front end
+	</title>
+	<body>
+		<!-- container for the page -->
+		<div class="container">
 
-		</div>
+			<div class="col-lg-8 text-center">
 
-		<div class="col-lg-12 text-offset">
-		<h1>Demo</h1>
 			</div>
-		<main>
 
-		<div class="col-lg-1 text-offset">
-        <img src="images/clock.jpeg" alt="clock">
+			<div class="col-lg-12 text-offset">
+
+				<h1>Demo</h1>
+			</div>
+			<main>
+
+				<div class="col-lg-1 text-offset">
+					<img src="images/clock.jpeg" alt="clock">
+				</div>
+			<div class="row">
+				<div class="col-lg-1 text-offset">
+					<img src="images/conditions.jpeg" alt="conditions">
+			</div>
+				<div class="notif">
+					<p>(Notification about air)</p>
+					<img src="images/notification.jpeg" alt="notifications">
+				</div>
+				</div>
+
+				<div class="greeting" alt="greeting">
+					<em> Good Morning Pi Group</em>
+				</div>
+
+				<div class="feed">
+					<img src="images/todolist.jpeg" alt="todolist">
+					<img src="images/slack.jpeg" alt="slack">
+				</div>
+
+			</main>
 		</div>
-			<div class="col-lg-1 text-offset">
-        <img src="images/conditions.jpeg" alt="conditions">
 		</div>
-        <div class="notif" >
-        <p>(Notification about air)</p>
-        <img src="images/notification.jpeg" alt="notifications">
-        </div>
-
-        <div class="greeting" alt="greeting">
-            <em> Good Morning Pi Group</em>
-        </div>
-
-        <div class="feed">
-        <img src="images/todolist.jpeg" alt="todolist">
-        <img src="images/slack.jpeg" alt="slack">
-        </div>
-
-    </main>
-		</div>
-    </div>
-</body>
+	</body>
 </html>
