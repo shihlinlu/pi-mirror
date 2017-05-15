f0d4a17f08e227ae59ad8ad3ecf2a68d072a27f6
S JACKSON <jackson.sabastian@gmail.com>
2017-05-15 16:44:35 -0600

    testting new front end

---
+++ epic/front-end-revision.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link type="text/css" href="css/style.css" rel="stylesheet"/>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
+
+	</head>
+	<body>
+		<!-- container for the page -->
+		<div class="row">
+			<div class="col-md-6">
+				<h1>
+					Pi-Mirror
+				</h1>
+			</div>
+			<div class="col-md-6">
+				<p><h1>4:20</h1></p>
+				<p><h1>MAY 16, 2017</h1></p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-6">
+				<img src="images/todolist.jpeg" alt="to-do">
+			</div>
+		</div>
+	</body>
+</html>
--- epic/staticFrEnd.php
+++ epic/staticFrEnd.php
@@ -1,20 +1,6 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="UTF-8">
-		<link type="text/css" href="css/style.css" rel="stylesheet"/>
-		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
-				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-				  crossorigin="anonymous"></script>
 
 	</head>
 
@@ -22,31 +8,33 @@
 		Static Front end
 	</title>
 	<body>
-		<!-- container for the page -->
-		<div class="container">
+			<!-- container for the page -->
+			<div class="container">
 
 			<div class="col-lg-8 text-center">
 
 			</div>
+			<div class="row">
 
-			<div class="col-lg-12 text-offset">
 
-				<h1>Demo</h1>
+				<div class="col-lg-12 text-offset">
+					<h1>Demo</h1>
+				</div>
 			</div>
 			<main>
-
 				<div class="row">
-				<div class = col-lg-1 text-offset">
+					<div class= "col-md-2 col-md-offset-5">
 					<img src="images/clock.jpeg" alt="clock">
+					</div>
 				</div>
-			<div class="row">
-				<div class="col-lg-1">
-					<img src="images/conditions.jpeg" alt="conditions">
-			</div>
-				<div class="notif">
-					<p>(Notification about air)</p>
-					<img src="images/notification.jpeg" alt="notifications">
-				</div>
+				<div class="row">
+					<div class="col-lg-1">
+						<img src="images/conditions.jpeg" alt="conditions">
+					</div>
+					<div class="notif">
+						<p>(Notification about air)</p>
+						<img src="images/notification.jpeg" alt="notifications">
+					</div>
 				</div>
 
 				<div class="greeting" alt="greeting">
@@ -57,9 +45,7 @@
 					<img src="images/todolist.jpeg" alt="todolist">
 					<img src="images/slack.jpeg" alt="slack">
 				</div>
-
 			</main>
-		</div>
 		</div>
 	</body>
 </html>
