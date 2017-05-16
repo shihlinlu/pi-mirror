9f2e4b42e25cba068e6a2fa795dc2d3dd7943eed
Luc Flynn <lucnmsu@gmail.com>
2017-05-16 13:14:02 -0600

    First attempt at html to do
    https://app.asana.com/0/333821720990064/333821720990069

---
+++ php/classes/Test/ReadingTest.php
@@ -0,0 +1,20 @@
+<?php
+namespace Edu\Cnm\PiMirror\Test;
+
+use Edu\Cnm\PiMirror\Sensor;
+
+// grab the class under scrutiny
+require_once(dirname(__DIR__) . "/autoload.php");
+
+/**
+ * FULL PHPUnit test for the Reading class
+ *
+ * This is a complete PHPUnit test for the Sensor class. It is complete because all mySQL/PDO enabled methods are tested for both invalid and valid inputs.
+ *
+ * @see Sensor
+ * @author Luc Flynn <lflynn7@cnm.edu>
+ **/
+class ReadingTest extends PiMirrorTest {
+
+
+}
