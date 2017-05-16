6fd2fe4594db727941737a924b567f6276a108be
Tucker L <tucker@fellafox.com>
2017-05-16 11:16:05 -0600

    Rebased for updated composer

---
+++ public_html/api/weather-api/index.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tucker
+ * Date: 5/10/2017
+ * Time: 2:07 PM
+ */
+
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+use Edu\Cnm\DataDesign\{
+	Tweet,
+	// we only use the profile class for testing purposes
+	Profile
+};
+
+/**
+ * api for the DarkSky Weather
+ **/
+
+//verify the session, start if not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+header("Content-type: application/json");
+if($reply->data === null) {
+	unset($reply->data);
+}
+
+// encode and return reply to front end caller
+echo json_encode($reply);
