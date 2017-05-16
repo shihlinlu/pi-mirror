cbae7ec7887dc76312dc1c4d15a3e603cc493e97
S JACKSON <jackson.sabastian@gmail.com>
2017-05-16 15:36:17 -0600

    fixed style - wtf happened ?:-0

--- epic/front-end-revision.php
+++ epic/front-end-revision.php
@@ -6,78 +6,59 @@
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
 				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
 				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
 		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
 				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
 				  crossorigin="anonymous"></script>
-
-        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css">
-        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
-        <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
-
 	</head>
 	<body>
 		<!-- container for the page -->
 		<div class="container">
-		<div class="row">
-			<div class="col-md-6">
-				<h1>
-					Pi-Mirror
+			<div class="row">
+				<div class="col-md-6">
+					<h1>
+						Pi-Mirror
+					</h1>
+				</div>
+				<div class="col-md-6">
+					<p><h1>4:20</h1></p>
+					<p><h1>MAY 16, 2017</h1></p>
+				</div>
+			</div>
+			<!-- to do list code -->
+			<div class="form-group">
+				<h1>To-Do
+					<small>List</small>
 				</h1>
+				<form role="form">
+					<input type="text" class="form-control" placeholder="Your Task" name="task">
+				</form>
+				<button type="button" class="btn btn btn-primary">Add</button>
 			</div>
-			<div class="col-md-6">
-				<p><h1>4:20</h1></p>
-				<p><h1>MAY 16, 2017</h1></p>
+			<ul class="list-unstyled" id="todo"></ul>
+			<!-- /to do list code -->
+			<div>
+				<ul class="list-unstyled" id="todo"></ul>
 			</div>
-		</div>
-		<!-- to do list code -->
-            <body>
-            <div class="form-group">
-                <h1>To-Do <small>List</small></h1>
-                <form role="form">
-                    <input type="text" class="form-control" placeholder="Your Task" name="task">
-                </form>
-                <button type="button" class="btn btn btn-primary">Add</button>
-            </div>
-            <div></div>
-            <ul class="list-unstyled" id="todo"></ul>
-            </body>
-                <!-- /to do list code -->
-            <div>
-                <ul class="list-unstyled" id="todo"></ul>
-            </div>
-			<div class="col-md-6">
-				<img src="images/slack.jpeg">
+			<div class="row">
+				<div class="col-md-6">
+					<img src="images/29353722-Pressure-gauge-dial-template-Stock-Vector.jpg" class="img-circle img-responsive sensors" alt="Responsive image">
+				</div>
+				<div class="col-md-6">
+					<img src="images/notification.jpeg">
+				</div>
 			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-6">
-				<img src="images/29353722-Pressure-gauge-dial-template-Stock-Vector.jpg" class="img-circle"  class="img-responsive" alt="Responsive image">
+			<div class="row">
+				<div class="col-md-6">
+					<img src="images/conditions.jpeg">
+				</div>
+				<div class="col-md-6">
+					<h1>Weather Data</h1>
+				</div>
 			</div>
-			<div class="col-md-6">
-				<img src="images/notification.jpeg">
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-6">
-				<img src="images/conditions.jpeg">
-			</div>
-			<div class="col-md-6">
-				<h1>Weather Data</h1>
-			</div>
-		</div>
-
-
-
-
-
-
-
-
 	</body>
 </html>
