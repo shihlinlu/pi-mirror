bc0506c8e2085f7b06804db8dd365b397ac6a921
Luc Flynn <lucnmsu@gmail.com>
2017-05-16 13:38:56 -0600

    began Reading class unit test
    https://app.asana.com/0/333821720990064/340160311802038

--- epic/front-end-revision.php
+++ epic/front-end-revision.php
@@ -36,12 +36,17 @@
 			</div>
 		</div>
 		<!-- to do list code -->
+            <body>
             <div class="form-group">
                 <h1>To-Do <small>List</small></h1>
                 <form role="form">
                     <input type="text" class="form-control" placeholder="Your Task" name="task">
                 </form>
                 <button type="button" class="btn btn btn-primary">Add</button>
+            </div>
+            <div></div>
+            <ul class="list-unstyled" id="todo"></ul>
+            </body>
                 <!-- /to do list code -->
             <div>
                 <ul class="list-unstyled" id="todo"></ul>
