7fe0ff003465ec6410cb3b0f432d64e2b2eb8d3f
Shihlin Lu <shihlinlu@gmail.com>
2017-05-17 09:22:46 -0600

    Minor corrections in docblock
    https://app.asana.com/0/333821720990064/336057981694068

--- php/classes/Test/SensorTest.php
+++ php/classes/Test/SensorTest.php
@@ -7,9 +7,9 @@ use Edu\Cnm\PiMirror\Sensor;
 require_once(dirname(__DIR__) . "/autoload.php");
 
 /**
- * FULL PHPUnit test for the Profile class
+ * FULL PHPUnit test for the Sensor class
  *
- * This is a complete PHPUnit test for the Sensor class. It is complete becase all mySQL/PDO enabled methods are tested for both invalid and valid inputs.
+ * This is a complete PHPUnit test for the Sensor class. It is complete because all mySQL/PDO enabled methods are tested for both invalid and valid inputs.
  *
  * @see Sensor
  * @author Shihlin Lu <slu5@cnm.edu>
