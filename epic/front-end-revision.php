63375343a6c106b00030a2dc66858f4537ab7ed7
Shihlin Lu <shihlinlu@gmail.com>
2017-05-17 13:30:47 -0600

    Updated front-end revision and css
    https://app.asana.com/0/333821720990064/336057981694068

--- epic/front-end-revision.php
+++ epic/front-end-revision.php
@@ -2,18 +2,37 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<link type="text/css" href="css/style.css" rel="stylesheet"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
 				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
 				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!-- [if lt IE9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- Custom CSS -->
+		<link rel="text/css" href="css/style.css" type="text/css"/>
+
+		<!-- jQuery v3.0 -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
+
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
 				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
 				  crossorigin="anonymous"></script>
+
+		<title>Front End- Static</title>
 	</head>
 	<body>
 		<!-- container for the page -->
@@ -52,7 +71,7 @@
 					<table class="table table-hover">
 						<tr>
 							<th>oxygen</th>
-							<th>Humidty</th>
+							<th>Humidity</th>
 						</tr>
 						<tr>
 							<td>EXAMPLE</td>
@@ -68,7 +87,7 @@
 					<img src="images/notification.jpeg">
 				</div>
 			</div>
-			<div class="row pad-down	">
+			<div class="row pad-down">
 				<div class="col-md-6">
 					<img src="images/conditions.jpeg">
 				</div>
