e0ef353736290eed1717ef7f9c15d605452d00eb
S JACKSON <jackson.sabastian@gmail.com>
2017-05-22 13:49:12 -0600

    added home wireframe version two to "wireframe-images" and pi_mirror_logo to "images" - both png files

--- epic/wireframes.php
+++ epic/wireframes.php
@@ -11,7 +11,7 @@
 		<main>
 			<!-- SUMMARY SECTION -->
 			<h2>Initial Sketches</h2>
-			<p>The initial wireframes are based on the standard features that the group collectively decided on, which includes temperature, time, current date, reminders, indoor air quality, and other user-input feeds (i.e., Slack, News). The user can navigate individually to each section through motion gestures; for instance, navigating to the indoor air quality will enable the user to access more information such as individual levels of gasses, detailed notifications, and charts. Next steps include research of the <a href="https://blog.prototypr.io/10-usability-heuristics-with-examples-4a81ada920c">10 Usability Heuristics of User Interface Design</a> to determine the default arrangement of modules.</p>
+			<p>The initial wireframes are based on the standard features that the group collectively decided on, which includes temperature, time, current date, reminders, indoor air quality, and other user-input feeds (i.e., Slack, News). The user can navigate individually to each section through motion gestures; for instance, navigating to the indoor air quality will enable the user to access more information such as individual levels of gasses, detailed notifications, and charts. Next steps include research of the <a href="https://blog.prototypr.io/10-usability-heuristics-with-examples-4a81ada920c">10 Usability Heuristics of User Interface Design</a> to determine the default arrangement of modules. With inspiration from the 10 examples and other mirrors the second wireframe was created with simplicity in mind </p>
 			<!-- SECTION 2 -->
 			<section>
 				<img src="wireframe-images/wireframes_v1.png" alt="home screen" />
