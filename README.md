e9193d0e467218c69ea601b5f8a2718f8ebef70a
S JACKSON <jackson.sabastian@gmail.com>
2017-05-25 16:54:31 -0600

    added home wireframe version two to "wireframe-images" and pi_mirror_logo to "images" - both png files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # pi-mirror-
 The magical touchscreen Raspberry Pi Smart Mirror. 
+5/25/2017 -  notes for front end 
+-- inject dummy data into placeholders 
+-- copy and paste this into angular notes
+-- 
--- epic/wireframes.php
+++ epic/wireframes.php
@@ -2,23 +2,30 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Wireframes 1.0</title>
+		<title>Wireframes </title>
 	</head>
 	<body>
 		<header>
-			<h1>Wireframes 1.0</h1>
+			<h1>Wireframes</h1>
 		</header>
 		<main>
 			<!-- SUMMARY SECTION -->
 			<h2>Initial Sketches</h2>
-			<p>The initial wireframes are based on the standard features that the group collectively decided on, which includes temperature, time, current date, reminders, indoor air quality, and other user-input feeds (i.e., Slack, News). The user can navigate individually to each section through motion gestures; for instance, navigating to the indoor air quality will enable the user to access more information such as individual levels of gasses, detailed notifications, and charts. Next steps include research of the <a href="https://blog.prototypr.io/10-usability-heuristics-with-examples-4a81ada920c">10 Usability Heuristics of User Interface Design</a> to determine the default arrangement of modules. With inspiration from the 10 examples and other mirrors the second wireframe was created with simplicity in mind </p>
+			<p>The initial wireframes are based on the standard features that the group collectively decided on, which
+				includes temperature, time, current date, reminders, indoor air quality, and other user-input feeds (i.e.,
+				Slack, News). The user can navigate individually to each section through motion gestures; for instance,
+				navigating to the indoor air quality will enable the user to access more information such as individual
+				levels of gasses, detailed notifications, and charts. Next steps include research of the <a
+					href="https://blog.prototypr.io/10-usability-heuristics-with-examples-4a81ada920c">10 Usability
+					Heuristics of User Interface Design</a> to determine the default arrangement of modules. With inspiration
+				from the first wireframe, "10 examples", and other smart mirrors the second wireframe was created with
+				simplicity in mind and the home
+				page as the Minimum Viable Project (MVP). </p>
 			<!-- SECTION 2 -->
 			<section>
-				<img src="wireframe-images/wireframes_v1.png" alt="home screen" />
-
+				<img src="wireframe-images/wireframes_v1.png" alt="v1">
+				<img src="wireframe-images/home-wireframe-v2.png" alt="v2">
 			</section>
-
-
 		</main>
 	</body>
 </html>
