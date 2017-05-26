62f3fb913b33e0eff706e33cd5b663b27a068888
S JACKSON <jackson.sabastian@gmail.com>
2017-05-26 12:33:22 -0600

    adding a new api for reading

--- public_html/api/readings/index.php
+++ public_html/api/readings/index.php
@@ -5,4 +5,27 @@
  * Date: 5/26/2017
  * Time: 11:38
  */
-require_once
+require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+use Edu\Cnm\PiMirror\Reading;
+/**
+ * API for Reading Class
+ * Sabastian Jackson w/ MatthewFischer
+ */
+// check the session status, if it is not active, start the session.
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+//create stdClass named $reply. this object will be used to store the results of the call to the api. sets status to 200 (success). creates data state variable, holds the result of the api call.
+try {
+	//grab the mySQL database connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/piomirrors.ini");
+	//determines which HTTP Method needs to be processed and stores the result in $method
+	$method = array_key_exists("HTTP_x_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+	//stores the Primary key for the GET methods in $id, This key will come in the URL sent by the front end. If no key is present $id will remain empty. Note that the input filtered.
+
