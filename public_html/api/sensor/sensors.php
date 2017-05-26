b2095e94dd6b4c41c94862423882509a5789c525
Shihlin Lu <shihlinlu@gmail.com>
2017-05-25 21:44:10 -0600

    added API skeleton for sensor and .htaccess
    https://app.asana.com/0/333821720990064/335386097235057

---
+++ public_html/api/sensor/sensors.php
@@ -0,0 +1,9 @@
+<?php
+require_once dirname(__DIR__,3)."/php/classes/autoload.php";
+require_once dirname(__DIR__,3)."/php/lib/xsrf.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+use Edu\Cnm\PiMirror\Profile;
+/**
+ * API for Sensors
+ * @author Shihlin Lu
+ */
