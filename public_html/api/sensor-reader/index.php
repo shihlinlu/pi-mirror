5c899005f38303baccc1dd69969248495b1e2536
Tucker L <tucker@fellafox.com>
2017-05-25 21:35:40 -0600

    Added files for additional API's sensor-reader and sensor-recorder. Also added folder for the python code.
    https://app.asana.com/0/333821720990064/335386097235057

---
+++ public_html/api/sensor-reader/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tucker
+ * Date: 5/25/2017
+ * Time: 3:47 PM
+ */
+
+/**
+ * These are notes. They can be erased but speak with Tucker first
+ *
+ * This will be a read-only api
+ *
+ */
---
+++ public_html/api/sensor-recorder/index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tucker
+ * Date: 5/25/2017
+ * Time: 3:35 PM
+ */
+
+/**
+ * These are notes that can be erased later (check with Tucker)
+ *
+ * Outline:
+ *[Python[SENSOR DATA]] --> [Python/PHP TRANSLATE VIA JSON] --> [PHP REST] --> [MY SQL]
+ *
+ * TASK: Create the following logic
+ * Only access when
+ * $SERVER["REMOTE_ADDR"]:
+ * 	local host
+ * 	127.0.0.1
+ * 	::1
+ *
+ * TASK: Disable XSRF as instructed by Dylan
+ *
+ */
