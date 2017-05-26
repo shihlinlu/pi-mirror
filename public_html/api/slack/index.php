384d8e061d893d52be6e1599a61cc6ed8820b578
Shihlin Lu <shihlinlu@gmail.com>
2017-05-25 21:53:47 -0600

    added exception and typeError
    https://app.asana.com/0/333821720990064/335386097235057

--- public_html/api/slack/index.php
+++ public_html/api/slack/index.php
@@ -5,7 +5,7 @@ require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 /**
- * api to display Slack channel events to the mirror
+ * API to display Slack channel events to the mirror
  *
  * @author Shihlin Lu
  * @author Luc Flynn
@@ -15,16 +15,29 @@ require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
+
 try {
 	// start session
 	if(session_status() !== PHP_SESSION_ACTIVE) {
 		session_start();
 	}
-	// grab mySQL statement
+	// grab mySQL connection
 	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/piomirrors.ini");
 
 	// determine which HTTP method is being used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
 
+
+
+
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+} catch(TypeError $typeError) {
+	$reply->status = $typeError->getCode();
+	$reply->message = $typeError->getMessage();
 }
+header("Content-type: application/json");
+echo json_encode($reply);
 
