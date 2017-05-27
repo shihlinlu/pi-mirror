7ad50de4ed4c966c5157a10ca19285db4aa103ef
S JACKSON <jackson.sabastian@gmail.com>
2017-05-26 22:21:19 -0600

    added input sanitization for first class
    https://s-media-cache-ak0.pinimg.com/originals/e5/8a/c7/e58ac733b2430952c83d2e05a024bd93.jpg

--- public_html/api/readings/index.php
+++ public_html/api/readings/index.php
@@ -20,6 +20,7 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
 //prepare an empty reply
 $reply = new stdClass();
 $reply->status = 200;
+//should it really be 200?
 $reply->data = null;
 //create stdClass named $reply. this object will be used to store the results of the call to the api. sets status to 200 (success). creates data state variable, holds the result of the api call.
 try {
@@ -27,5 +28,9 @@ try {
 	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/piomirrors.ini");
 	//determines which HTTP Method needs to be processed and stores the result in $method
 	$method = array_key_exists("HTTP_x_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+	//sanitize input
+	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+	$tweetProfileId = filter_input(INPUT_GET, "tweetProfileId", FILTER_VALIDATE_INT);
+	$tweetContent = filter_input(INPUT_GET, "tweetContent", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	//stores the Primary key for the GET methods in $id, This key will come in the URL sent by the front end. If no key is present $id will remain empty. Note that the input filtered.
 
