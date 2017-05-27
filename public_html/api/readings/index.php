db57bcd1f481231e93f4386ee02908af1610056a
S JACKSON <jackson.sabastian@gmail.com>
2017-05-27 12:04:40 -0600

    added fet for sensor reading id

--- public_html/api/readings/index.php
+++ public_html/api/readings/index.php
@@ -9,6 +9,7 @@ require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 use Edu\Cnm\PiMirror\Reading;
+
 /**
  * API for Reading Class
  * Sabastian Jackson w/ MatthewFischer
@@ -28,34 +29,25 @@ try {
 	//determines which HTTP Method needs to be processed and stores the result in $method
 	$method = array_key_exists("HTTP_x_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 	//stores the Primary key for the GET methods in $id, This key will come in the URL sent by the front end. If no key is present $id will remain empty. Note that the input filtered.
-//make sure the id is valid for methods that require it
-	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
-		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
+	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+	//add a start mandate after?
+	$sensorDateTime = filter_input(INPUT_GET, "sensorDateTime");
+	$pageNum = filter_input(INPUT_GET, "pageNum", FILTER_SANITIZE_NUMBER_INT);
+	//page number
+	if(empty($pageNum) === true || $pageNum < 0) {
+		$pageNum = 0;
 	}
-	<?php
-if($method === "GET") {
-	//set XSRF cookie
-	setXsrfCookie();
 
-	//get a specific tweet based on arguments provided or all the tweets and update reply
-	if(empty($id) === false) {
-		$tweet = Tweet::getTweetByTweetId($pdo, $id);
-		if($tweet !== null) {
-			$reply->data = $tweet;
-		}
-	} else if(empty($tweetProfileId) === false) {
-		$tweet = Tweet::getTweetByTweetProfileId($pdo, $tweetProfileId)->toArray();
-		if($tweet !== null) {
-			$reply->data = $tweet;
-		}
-	} else if(empty($tweetContent) === false) {
-		$tweets = Tweet::getTweetByTweetContent($pdo, $tweetContent)->toArray();
-		if($tweets !== null) {
-			$reply->data = $tweets;
-		}
-	} else {
-		$tweets = Tweet::getAllTweets($pdo)->toArray();
-		if($tweets !== null) {
-			$reply->data = $tweets;
+//here we determine if the request received is a GET request
+	if($method === "GET") {
+		//set XSRF cookie
+		setXsrfCookie("/");
+		//handle GET request - if id is present, that checkbook value is present, that checkbook value is returned, otherwise all values are returned.
+		//determine is a Key was sent in the URL by checking $id. if so we pull the requested checkbook value by checkbook ID from the database and store it in $checkbook
+		if(empty($id) === false) {
+			$reply->data = Reading::getReadingByReadingSensorId($pdo, $id);
+		} elseif(empty($sensorDateTime) === false) {
+			reply->data =
 		}
 	}
+}
