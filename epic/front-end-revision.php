73c4df5f3c0cfd6292232106adf0870ecd723c2c
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-06-05 16:30:28 -0600

    changed the static-front end inverted color with css, added font awesome, got ride of images and added a php time and data function

--- epic/front-end-revision.php
+++ epic/front-end-revision.php
@@ -3,10 +3,16 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+		<!--font awesome for icons and stuff-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
 				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
 		<!-- Optional theme -->
 		<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
 				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous"> -->
@@ -17,22 +23,24 @@
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
 		<!-- Custom CSS -->
-		<link rel="text/css" href="css/style.css" type="text/css"/>
+		<link rel="stylesheet" href="css/style2.css" type="text/css"/>
 		<!-- jQuery v3.0 -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
 				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
 				  crossorigin="anonymous"></script>
-		<!-- as of Saturday May 20th, it was noticed that css was not effecting anything -->
+
 		<title>Front End- Static</title>
 	</head>
+
+
 	<body>
 		<!-- container for the page -->
 		<div class="container">
 			<div class="row">
 				<!-- notification section -->
 				<div class="col-md-3">
-					<img src="images/notification.png" class="inverted">
+					<i class="fa fa-sun-o" fa-20px></i>
 				</div>
 				<div class="col-md-4 text-right">
 					<!-- welcome message -->
@@ -41,31 +49,13 @@
 					</h1>
 				</div>
 				<!-- date, time, and weather section -->
-				<div class="col-md-3 col-md-offset-2 col-xs-4 col-xs-offset-4">
-					<img src="images/date.png" alt="date" width="200" height="200" class="inverted">
-					<img src="images/time.png" class="float-right" alt="time" width="200" height="200" class="inverted">
-					<img src="images/gusty.png" width="200" height="200" class="inverted">
-				</div>
-			</div>
-			<!-- to do list -->
-			<div class="row pad-down">
-				<div class="form-group col-md-3 col-md-offset-9">
-					<h1>To-Do
-						<small>List</small>
-					</h1>
-					<form role="form">
-						<input type="text" class="form-control" placeholder="Your Task" name="task">
-					</form>
-					<button type="button" class="btn btn btn-primary">Add</button>
-				</div>
-				<!-- Insert slack feed here -->
-				<div class="col-md-6 col-md-offset-6 text-right">
-					<p>Slack this</p>
-					<p>Slack that</p>
-					<p>Slack you</p>
+				<?php
+				$t=time();
+				echo($t . "<br>");
+				echo(date("Y-m-d",$t));
+				?>
 				</div>
 			</div>
 
-		</div>
 	</body>
 </html>
