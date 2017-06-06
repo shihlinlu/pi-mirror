793f12ae593458611996416a53de6abd90ea8ee5
Shihlin Lu <shihlinlu@gmail.com>
2017-06-06 12:06:53 -0600

    corrected namespace in slack api; updated api methods

--- public_html/api/slack/index.php
+++ public_html/api/slack/index.php
@@ -4,7 +4,8 @@ require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
 require_once (dirname(__DIR__, 3) . "/vendor/autoload.php");
 
-use CL\Slack;
+
+use CL\Slack\Payload\ChannelsHistoryPayload;
 
 
 /**
@@ -23,6 +24,8 @@ use CL\Slack;
 	$reply->status = 200;
 	$reply->data = null;
 
+
+
 try {
 
 	// initialize encrypted config variable
@@ -37,29 +40,34 @@ try {
 	// determine which HTTP method is being used
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
+
 	if ($method === "GET") {
-		// set xsrf
+		//set xsrf
 		setXsrfCookie();
 
-		// gets the latest message from the specified Slack channel
-		if(empty($id) === false) {
-			$slack = Slack::$message($pdo, $message);
-			if($slack !== null) {
-				$reply->data = $message;
-			}
+		// specify slack channel for message retrieval
+		$payload = new ChannelsHistoryPayload();
+		$payload->setChannelId('C5BGUJ6R0');
+
+		$apiClient = new \CL\Slack\Transport\ApiClient("slack2");
+		$response = $apiClient->send($payload);
+
+		if($response->isOk()) {
+			//channel with latest history
+			$response->getLatest();
+		} else {
+			// error message
+			echo $response->getError();
+
+			// error explanation
+			echo $response->getErrorExplanation();
 		}
 
-		$slack = new SlackFeed($slack2);
-
-		// specify the channel to get the latest message from
-		$message = $slack->get('C5BGUJ6R0');
-
-		if(empty($message->currently) === true) {
-			throw new \RuntimeException("Can't retrieve the latest message", $slack->status);
-		}
 	} else {
 		throw (new \InvalidArgumentException("invalid http method request"));
 
+
+		// if an exception is thrown update the
 	}
 } catch(Exception $exception) {
 	$reply->status = $exception->getCode();
@@ -67,6 +75,7 @@ try {
 } catch(TypeError $typeError) {
 	$reply->status = $typeError->getCode();
 	$reply->message = $typeError->getMessage();
+
 }
 header("Content-type: application/json");
 echo json_encode($reply);
