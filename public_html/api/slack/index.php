75bd1d3ebffb078dbafb303b25e794684c8185c8
Shihlin Lu <shihlinlu@gmail.com>
2017-06-06 09:52:37 -0600

    updated slack api
    https://app.asana.com/0/332296370771689/335386097235057

--- public_html/api/slack/index.php
+++ public_html/api/slack/index.php
@@ -4,7 +4,8 @@ require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
 require_once (dirname(__DIR__, 3) . "/vendor/autoload.php");
 
-use Slack\Slack;
+use CL\Slack;
+
 
 /**
  * API to display Slack channel history to the mirror
@@ -31,31 +32,35 @@ try {
 	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/piomirrors.ini");
 
 	// variable that will house the API key for Slack API
-	$slack = $config["slack"];
+	$slack2 = $config["slack2"];
 
 	// determine which HTTP method is being used
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
-	//If method is a GET request, return the latest message
-	$payload = new ChannelsHistoryPayload();
-	$payload->setChannelId('C5BGUJ6R0');
+	if ($method === "GET") {
+		// set xsrf
+		setXsrfCookie();
 
-	$apiClient = new ApiClient('slack-token-here');
-	$response = $apiClient->send($payload);
+		// gets the latest message from the specified Slack channel
+		if(empty($id) === false) {
+			$slack = Slack::$message($pdo, $message);
+			if($slack !== null) {
+				$reply->data = $message;
+			}
+		}
 
+		$slack = new SlackFeed($slack2);
 
+		// specify the channel to get the latest message from
+		$message = $slack->get('C5BGUJ6R0');
 
-
-	if($response->isOk()) {
-		$response->getLatest()
+		if(empty($message->currently) === true) {
+			throw new \RuntimeException("Can't retrieve the latest message", $slack->status);
+		}
 	} else {
-		// simple error
-		echo $response->getError();
+		throw (new \InvalidArgumentException("invalid http method request"));
 
-		// explained error
-		echo $response->getErrorExplanation();
 	}
-
 } catch(Exception $exception) {
 	$reply->status = $exception->getCode();
 	$reply->message = $exception->getMessage();
@@ -66,56 +71,3 @@ try {
 header("Content-type: application/json");
 echo json_encode($reply);
 
-
-/**
- * This is work-in-progress code for the slack-string to MySQL logic
- * @author Tucker Logan
-
-
-// variable where string from slack will be stored
-$slackString = "!todo boomshakalanaasas"; //This variable will be filled with the string from slack.
-
-//makes sure that the string from slack contains a possible bang-tag
-if(preg_match('#!#',$slackString) === 1 ) {
-	//switch statement that filters the string for the various band-tag options
-	//it then executes the case's code and places the string into the appropriate MySQL table
-	switch($slackString) {
-
-		case (preg_match('#!treky#',$slackString) === 1);
-			echo "treky"; //this is simply for testing
-			//MySQL injection code will go here
-			break;
-		case (preg_match('#!challenge#',$slackString) === 1);
-			echo "challenge"; //this is simply for testing
-			//MySQL injection code will go here
-			break;
-		case (preg_match('#!todo#',$slackString) === 1);
-			echo "todo"; //this is simply for testing
-			//MySQL injection code will go here
-			break;
-		case (preg_match('#!snackbot#',$slackString) === 1);
-			echo "snackbot"; //this is simply for testing
-			//MySQL injection code will go here
-			break;
-		case (preg_match('#!pun#',$slackString) === 1);
-			echo "pun counter"; //this is simply for testing
-			//MySQL injection code will go here
-			break;
-		case (preg_match('#!tictactoe#',$slackString) === 1);
-			echo "tictactoe"; //this is simply for testing
-			//MySQL injection code will go here
-			break;
-		case (preg_match('#!easteregg#',$slackString) === 1);
-			echo "easteregg"; //this is simply for testing
-			//MySQL injection code will go here
-			break;
-		case (preg_match('#!announce#',$slackString) === 1);
-			echo "announce"; //this is simply for testing
-			//MySQL injection code will go here
-			break;
-		//default if there was a "!" in the string; however, it did not match our band-tag requirements
-		default;
-			echo "String did not match a switch case!";
-	}
-}
- */
