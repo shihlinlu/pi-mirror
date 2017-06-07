a967b3d8239de4263021a15dd95b2f8b0b92f7dd
Luc Flynn <lucnmsu@gmail.com>
2017-06-07 16:18:59 -0600

    added status class https://app.asana.com/0/333821720990064/340160311802038

--- src/app/services/weather-service.ts
+++ src/app/services/weather-service.ts
@@ -2,15 +2,19 @@ import {Injectable} from "@angular/core";
 import {Http} from "@angular/http";
 import {Observable} from "rxjs/Observable";
 import {Weather} from "../classes/weather";
+import {BaseService} from "./base-service";
 
 @Injectable()
-export class WeatherService {
-    constructor(protected http: Http) {}
+export class WeatherService extends BaseService {
+    constructor(protected http: Http) {
+        super(http);
+    }
 
     private weatherUrl = "api/weather/";
 
     getWeather() : Observable<Weather> {
         return(this.http.get(this.weatherUrl)
-            .map(response => response.json()));
+            .map(this.extractData)
+            .catch(this.handleError));
     }
 }
