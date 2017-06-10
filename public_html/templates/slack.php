1416d95b2dfe976170abd391b53e72dce21b809e
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-06-10 15:35:36 -0600

    added html and css to templates slack.php and weather.php and app.css to make it prettier

--- public_html/templates/slack.php
+++ public_html/templates/slack.php
@@ -1,9 +1,23 @@
-<h2>Slack</h2>
-<div class="slack">
-	<ul>
-		<li>type: {{ slack.type }}</li>
-		<li>user: {{ slack.user }}</li>
-		<li>text: {{ slack.text }}</li>
-		<li>time: {{ slack.time }}</li>
-	</ul>
+
+
+
+
+<div class="container slack">
+	<div class="col-md-4">
+		<!-- slack message -->
+		<h1>Slack</h1> <i class="fa fa-slack fa-4x"></i>
+		<div class="messages">
+			<div class="col-md-6">
+				<h2>Slack</h2>
+				<div class="slack">
+					<ul>
+						<li>type: {{ slack.type }}</li>
+						<li>user: {{ slack.user }}</li>
+						<li>text: {{ slack.text }}</li>
+						<li>time: {{ slack.time }}</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
--- public_html/templates/weather.php
+++ public_html/templates/weather.php
@@ -1,13 +1,19 @@
-<h1>Weather in Albuquerque</h1>
-    <div class="currently">
-        <div class="col-md-6">
-            <ul>
-                <li>time: {{ weather.time | date }}</li>
-                <li>temperature: {{ weather.temperature }}</li>
-                <li>apparentTemperature: {{ weather.apparentTemperature }}</li>
-                <li>windSpeed: {{ weather.windSpeed }}</li>
-                <li>windBearing: {{ weather.windBearing }}</li>
-                <li>Summary: {{ weather.summary }}</li>
-            </ul>
-        </div>
-    </div>
+
+<div class="row">
+	<div class="container weather">
+		<div class="col-md-4">
+			<h1>Weather in Albuquerque</h1>
+			<div class="currently">
+				<div class="col-md-6">
+					<ul>
+						<li>time: {{ weather.time | date }}</li>
+						<li>temperature: {{ weather.temperature }}</li>
+						<li>apparentTemperature: {{ weather.apparentTemperature }}</li>
+						<li>windSpeed: {{ weather.windSpeed }}</li>
+						<li>windBearing: {{ weather.windBearing }}</li>
+						<li>Summary: {{ weather.summary }}</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
