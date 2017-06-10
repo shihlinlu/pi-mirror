c251e157bcd9952b540de3aeb9479a5dfde0f8cd
Luc Flynn <lucnmsu@gmail.com>
2017-06-10 17:23:22 -0600

    solved some issues on the front end

--- public_html/templates/slack.php
+++ public_html/templates/slack.php
@@ -3,20 +3,16 @@
 
 
 <div class="container slack">
-	<div class="col-md-4">
+	<div class="col-md-8">
 		<!-- slack message -->
-		<h1>Slack</h1> <i class="fa fa-slack fa-4x"></i>
+		<h1>Slack <i class="fa fa-slack fa-2x"></i></h1>
 		<div class="messages">
-			<div class="col-md-6">
-				<h2>Slack</h2>
-				<div class="slack">
+
 					<ul>
 						<li>user: {{ slack.user }}</li>
 						<li>text: {{ slack.text }}</li>
 						<li>time: {{ slack.ts }}</li>
 					</ul>
-				</div>
 			</div>
-		</div>
 	</div>
 </div>
