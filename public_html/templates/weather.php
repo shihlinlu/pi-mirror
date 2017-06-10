5a22db73fb55c0d2077d0d322dca89ac89dbcc2c
Luc Flynn <lucnmsu@gmail.com>
2017-06-10 17:32:24 -0600

    added date

--- public_html/templates/weather.php
+++ public_html/templates/weather.php
@@ -5,7 +5,7 @@
 			<h1>Weather in Albuquerque</h1>
 			<div class="currently">
 					<ul>
-						<li>time: {{ weather.time | date }}</li>
+						<li>Date: {{ weather.time | date }}</li>
 						<li>temperature: {{ weather.temperature }}</li>
 						<li>apparentTemperature: {{ weather.apparentTemperature }}</li>
 						<li>windSpeed: {{ weather.windSpeed }}</li>
