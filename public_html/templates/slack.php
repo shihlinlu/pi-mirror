094d8ae6b4c2874325215d2c1d7f18d4a01e4fc3
Shihlin Lu <shihlinlu@gmail.com>
2017-06-10 15:07:08 -0600

    updated slack template

--- public_html/templates/slack.php
+++ public_html/templates/slack.php
@@ -1,7 +1,9 @@
 <h2>Slack</h2>
 <div class="slack">
-    <ul>
-        <li>type: {{ slack.type }}</li>
-        <li>user: {{ slack.user }}</li>
-    </ul>
+	<ul>
+		<li>type: {{ slack.type }}</li>
+		<li>user: {{ slack.user }}</li>
+		<li>text: {{ slack.text }}</li>
+		<li>time: {{ slack.time }}</li>
+	</ul>
 </div>
--- public_html/templates/weather.php
+++ public_html/templates/weather.php
@@ -8,7 +8,6 @@
                 <li>windSpeed: {{ weather.windSpeed }}</li>
                 <li>windBearing: {{ weather.windBearing }}</li>
                 <li>Summary: {{ weather.summary }}</li>
-                <li>user: {{ slack.user }} </li>
             </ul>
         </div>
     </div>
