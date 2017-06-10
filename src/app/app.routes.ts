aae24370c4b20b47bc150d8d5005a4bf94e7f9f3
Shihlin Lu <shihlinlu@gmail.com>
2017-06-10 15:29:32 -0600

    removed bacon component, template, and service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,16 @@
 import {RouterModule, Routes} from "@angular/router";
-import {BaconComponent} from "./components/bacon-component";
 import {HomeComponent} from "./components/home-component";
-import {BaconService} from "./services/bacon-service";
 import {WeatherService} from "./services/weather-service";
 import {WeatherComponent} from "./components/weather-component";
 import {SlackComponent} from "./components/slack-component";
 import {SlackService} from "./services/slack-service";
 
-export const allAppComponents = [BaconComponent, HomeComponent, WeatherComponent, SlackComponent];
+export const allAppComponents = [HomeComponent, WeatherComponent, SlackComponent];
 
 export const routes: Routes = [
-	{path: "bacon", component: BaconComponent},
 	{path: "", component: HomeComponent}
 ];
 
-export const appRoutingProviders: any[] = [BaconService, WeatherService, SlackService];
+export const appRoutingProviders: any[] = [WeatherService, SlackService];
 
 export const routing = RouterModule.forRoot(routes);
