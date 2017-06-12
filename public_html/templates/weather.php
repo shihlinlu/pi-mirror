0cc010e40ba433c2fa812c1a3aaf3db698065476
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-06-12 09:54:07 -0600

    added images to public html and added code from static front end to weather templates npm run build and checked it on the front end and it works!!! slack and weather are in different rows though need to fix that

--- public_html/templates/weather.php
+++ public_html/templates/weather.php
@@ -1,17 +1,20 @@
 
 <div class="row">
 	<div class="container weather">
-		<div class="col-md-4">
-			<h1>Weather in Albuquerque</h1>
+		<div class="col-sm-8">
+			<h1>Weather in Albuquerque<i class="fa fa-sun-o fa-pulse fa-2p fa-fw"></i><span class="sr-only">Loading...</span></h1>
+
 			<div class="currently">
-					<ul>
-						<li>Date: {{ weather.time | date }}</li>
-						<li>temperature: {{ weather.temperature }}</li>
-						<li>apparentTemperature: {{ weather.apparentTemperature }}</li>
-						<li>windSpeed: {{ weather.windSpeed }}</li>
-						<li>windBearing: {{ weather.windBearing }}</li>
-						<li>Summary: {{ weather.summary }}</li>
-					</ul>
-				</div>
+				<ul class="fa-ul">
+					<li><i class="fa-li fa fa-clock-o"> </i>time: {{ weather.time | date }}</li>
+					<li><i class="fa-li fa fa-thermometer-three-quarters"></i>temperature: {{ weather.temperature }}</li>
+					<li><i class="fa-li fa fa-thermometer-full"></i>apparentTemperature: {{ weather.apparentTemperature }}</li>
+					<li><i class="fa-li fa fa-skyatlas"></i>windSpeed: {{ weather.windSpeed }}</li>
+					<li><i class="fa-li fa fa-mixcloud"></i>windBearing: {{ weather.windBearing }}</li>
+					<li><i class="fa-li fa fa-sun-o"></i>Summary: {{ weather.summary }}</li>
+				</ul>
+				<img src="images2/{{weather.icon}}">
+			</div>
 		</div>
 	</div>
+
