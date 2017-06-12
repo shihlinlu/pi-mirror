733d92c27bf4e8a3eec492db77b2fcf9941aa852
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-06-12 09:41:58 -0600

    added front end code to slack template npm run build and it works

--- public_html/templates/slack.php
+++ public_html/templates/slack.php
@@ -1,18 +1,17 @@
 
 
 
-
 <div class="container slack">
-	<div class="col-md-8">
+	<div class="col-md-6">
 		<!-- slack message -->
 		<h1>Slack <i class="fa fa-slack fa-2x"></i></h1>
 		<div class="messages">
 
-					<ul>
-						<li>user: {{ slack.user }}</li>
-						<li>text: {{ slack.text }}</li>
-						<li>time: {{ slack.ts }}</li>
-					</ul>
-			</div>
+			<ul class="fa-ul">
+				<li><i class="fa-li fa fa-user"></i>user: {{ slack.user }}</li>
+				<li><i class="fa-li fa fa-envelope-o"></i>text: {{ slack.text }}</li>
+				<li><i class="fa-li fa fa-clock-o"></i>time: {{ slack.ts }}</li>
+			</ul>
+		</div>
 	</div>
 </div>
