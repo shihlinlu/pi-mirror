ea692ff07acf28065df4e453d13d2d6bf06ac7e4
Shihlin Lu <shihlinlu@gmail.com>
2017-06-13 20:01:02 -0600

    blaming @DameonSmith and @karstagg for utter incompetence :pouting_cat: :scream_cat:

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -44,7 +44,8 @@ module.exports = {
 		new webpack.ProvidePlugin({
 			$: "jquery",
 			jQuery: "jquery",
-			"window.jQuery": "jquery"
+			"window.jQuery": "jquery",
+			PubNub: "pubnub"
 		}),
 
 		new HtmlWebpackPlugin({
