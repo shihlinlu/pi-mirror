0a616ced2aa74701ade28a0cd182f23b7e9d8523
Shihlin <shihlinlu@gmail.com>
2018-05-30 21:28:46 -0600

    Updated dependencies, switched to yarn

--- public_html/index.php
+++ public_html/index.php
@@ -10,16 +10,10 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<base href="<?php echo dirname($_SERVER["PHP_SELF"]) . "/"; ?>" />
+		<script type="text/javascript" src="https://cdn.pubnub.com/sdk/javascript/pubnub.4.10.0.min.js"></script>
 
 		<title>Pi Mirror</title>
-		<link href="dist/vendor.8fa53fcde0c9a011cdad.css" rel="stylesheet">
-		<link href="dist/css.8fa53fcde0c9a011cdad.css" rel="stylesheet">
-		<script type="text/javascript" src="dist/polyfills.8fa53fcde0c9a011cdad.js"></script>
-		<script type="text/javascript" src="dist/vendor.8fa53fcde0c9a011cdad.js"></script>
-		<script type="text/javascript" src="dist/app.8fa53fcde0c9a011cdad.js"></script>
-		<script type="text/javascript" src="dist/css.8fa53fcde0c9a011cdad.js"></script>
-
-	</head>
+	<link href="dist/vendor.6111f6941752c2c37d88.css" rel="stylesheet"><link href="dist/css.6111f6941752c2c37d88.css" rel="stylesheet"><script type="text/javascript" src="dist/polyfills.6111f6941752c2c37d88.js"></script><script type="text/javascript" src="dist/vendor.6111f6941752c2c37d88.js"></script><script type="text/javascript" src="dist/app.6111f6941752c2c37d88.js"></script><script type="text/javascript" src="dist/css.6111f6941752c2c37d88.js"></script></head>
 	<body>
 		<pi-mirror>Loading&hellip;</pi-mirror>
 	</body>
